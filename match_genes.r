d30b854f122a55acfb7fc7e59c0110d17deeb243
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-11 19:00:57 +0100

    Delete match_genes.r

--- match_genes.r
+++
@@ -1,8 +0,0 @@
-match_genes <- function(tuGenes_vector, protein_info_data) {
-   
-    matched_genes <- protein_info_data[protein_info_data[, 2] %in% tuGenes_vector, ]
-    
-    #string_protein_id (colonna 1) e preferred_name (colonna 2)
-    result <- matched_genes[, c(1, 2)]
-    return(result)
-  }
