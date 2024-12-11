f2b64b67ee89c0c06623760b60e705d4babc5399
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-11 19:01:22 +0100

    Delete tuGenes_filter.r

--- tuGenes_filter.r
+++
@@ -1,9 +0,0 @@
-# Funzione per filtrare la colonna X5.tuGenes
-  tuGenes_filter <- function(x) {
-    if ("X5.tuGenes" %in% colnames(x)) {
-      x$X5.tuGenes <- sapply(strsplit(as.character(x$X5.tuGenes), ";"), `[`, 1)
-    } else {
-      stop("La colonna 'X5.tuGenes' non esiste")
-    }
-    return(x)
-  }
