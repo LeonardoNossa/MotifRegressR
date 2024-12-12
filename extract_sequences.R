f941d9564cd1d38c49fbc3c6bd62cdd62203bda1
Elena <129421537+elenasasso@users.noreply.github.com>
2024-12-12 11:34:38 +0100

    Update extract_sequences.R with roxygen

--- extract_sequences.R
+++ extract_sequences.R
@@ -1,3 +1,22 @@
+#' Extract DNA Sequences from a FASTA Object
+#'
+#' This function extracts a DNA sequence from a FASTA object based on the 
+#' provided chromosome, start, and end positions. It returns the sequence along
+#' with its associated chromosome and gene name.
+#'
+#' @param FASTA A named list or vector of DNA sequences, where each element is
+#' a sequence.
+#' @param row A vector containing the chromosome name, start position, and 
+#' end position of the sequence to extract. The row should be in the 
+#' format: c(chr, start, end).
+#'
+#' @return A character vector with three elements:
+#' \item{chromosome}{The name of the chromosome from which the sequence is 
+#' extracted.}
+#' \item{gene_name}{The gene name, extracted from the row names.}
+#' \item{sequence}{The extracted DNA sequence.}
+#'
+#' @export
 extract_sequences <- function(FASTA,row){
   chr <- row[1]
   start <- row[2]
