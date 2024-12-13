d202affc98b4aa1aee326eaddac2150b5cfb912a
Elena <129421537+elenasasso@users.noreply.github.com>
2024-12-13 10:07:45 +0100

    Update tuGenes_filter.R with roxygen and English

--- tuGenes_filter.R
+++ tuGenes_filter.R
@@ -1,9 +1,20 @@
-# Funzione per filtrare la colonna X5.tuGenes
-  tuGenes_filter <- function(x) {
-    if ("X5.tuGenes" %in% colnames(x)) {
-      x$X5.tuGenes <- sapply(strsplit(as.character(x$X5.tuGenes), ";"), `[`, 1)
-    } else {
-      stop("La colonna 'X5.tuGenes' non esiste")
-    }
-    return(x)
+#' Filter the `X5.tuGenes` Column of a Data Frame
+#'
+#' This function processes the `X5.tuGenes` column of a data frame by 
+#' extracting the portion before the first semicolon (`;`).
+#' If the column does not exist, the function stops with an error.
+#'
+#' @param x A data frame containing a column named `X5.tuGenes`.
+#'
+#' @return A data frame with the `X5.tuGenes` column updated to include only 
+#' the portion before the first semicolon (`;`).
+#'
+#' @export
+tuGenes_filter <- function(x) {
+  if ("X5.tuGenes" %in% colnames(x)) {
+    x$X5.tuGenes <- sapply(strsplit(as.character(x$X5.tuGenes), ";"), `[`, 1)
+  } else {
+    stop("The column 'X5.tuGenes' does not exist")
   }
+  return(x)
+}
