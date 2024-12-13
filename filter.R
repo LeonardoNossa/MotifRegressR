cd9300adf92b5497f21d07822cce31ef1516f53d
Elena <129421537+elenasasso@users.noreply.github.com>
2024-12-13 10:09:46 +0100

    Update filter.R with right spaces for roxygen

--- filter.R
+++ filter.R
@@ -19,6 +19,7 @@ filter <- function(to_filter, to_keep){
   ids_to_keep <- unlist(to_keep$Gene_ID)
   names_to_keep <- to_keep$Gene_Name
   filtered <- to_filter[rownames(to_filter)%in%ids_to_keep,]
-  rownames(filtered) <- names_to_keep[which(rownames(filtered) %in% ids_to_keep)]
+  rownames(filtered) <- names_to_keep[which(rownames(filtered) %in% 
+                                              ids_to_keep)]
   return(filtered)
 }
