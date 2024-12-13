dec8b4e4112aa58bdb3adcd39232535970104a24
Elena <129421537+elenasasso@users.noreply.github.com>
2024-12-13 14:36:43 +0100

    Update principal_comps.R with final roxygen

--- principal_comps.R
+++ principal_comps.R
@@ -1,14 +1,26 @@
-#' principal_comps
+#' Perform Principal Component Analysis (PCA)
 #'
-#' @param matrix 
-#' @param explained_var 
-#' @param axis 
-#' @param scale 
+#' This function performs Principal Component Analysis (PCA) on a given matrix 
+#' and extracts the components needed to explain a specified percentage of the 
+#' variance.
 #'
-#' @return
+#' @param matrix A numeric matrix to perform PCA on. Each row represents an 
+#' observation, and each column represents a variable. If `axis = 1`, rows and 
+#' columns are transposed.
+#' @param explained_var A numeric value (default: 95) indicating the percentage
+#' of variance that the retained principal components should explain.
+#' @param axis An integer (default: 2) indicating whether to analyze the input 
+#' matrix as is (`axis = 2`) or transpose it (`axis = 1`).
+#' @param scale A logical value (default: TRUE) indicating whether the variables
+#' should be scaled to unit variance before performing PCA.
+#'
+#' @return A numeric matrix containing the coordinates of the input data 
+#' projected onto the principal components that explain the specified variance. 
+#' The output matrix has dimensions consistent with the transposition specified 
+#' by `axis`.
+#'
 #' @import FactoMineR
 #' @export
-#' @examples
 principal_comps <- function(matrix,explained_var = 95, axis = 2, scale = TRUE){
   
   if(!(is.matrix(matrix))){
