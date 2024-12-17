9e4c58effd567c352cf44ff450490a79852ed3df
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-17 19:15:04 +0100

    Update regressor
    regressor has been updated: now the function automatically converts the S matrix to a df and we only make regression on the rows shared by compendium and S

--- regressor.R
+++ regressor.R
@@ -1,5 +1,11 @@
 regressor <- function(scores,compendium,conditions,regression_type){
 
+  scores <- as.data.frame(scores)
+
+  common_rownames <- intersect(rownames(scores),rownames(compendium))
+  scores <- scores[common_rownames, , drop = FALSE]
+  compendium <- compendium[common_rownames, , drop = FALSE]
+
   output <- list()
   for (regression in regression_type){
     regression_models <- train_models(scores, compendium, conditions, regression)
