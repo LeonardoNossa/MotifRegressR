8f32e21247e87d95b1c25adfe11106970f069a8d
Elena <129421537+elenasasso@users.noreply.github.com>
2024-12-18 23:12:23 +0100

    Update unpack_svm.R with roxygen

--- unpack_svm.R
+++ unpack_svm.R
@@ -1,10 +1,29 @@
+#' Unpack svm
+#'
+#' This function extracts the top `k` most important motifs from a 
+#' trained SVM model based on their feature importance.
+#'
+#' @param svm_sublist A list containing a single SVM model (object of 
+#' class `svm`).
+#' @param k An integer specifying the number of top features to extract.
+#'
+#' @return A data frame with the following columns:
+#' \itemize{
+#'   \item `motifs`: The names of the top motifs.
+#'   \item `conditions`: The name of the condition corresponding to the motifs.
+#'   \item `coef`: The feature importance values for the motifs.
+#' }
+#'
+#' @export
 unpack_svm <- function(svm_sublist, k){
   svm_model <- svm_sublist[[1]]
   weights <- t(svm_model$coefs) %*% svm_model$SV
   
   feature_importance <- apply(weights, 2, function(x) sqrt(sum(x^2)))
   
-  top_k_indices <- order(feature_importance, decreasing = TRUE)[1:min(k, length(feature_importance))]
+  top_k_indices <- 
+    order(feature_importance, 
+          decreasing = TRUE)[1:min(k, length(feature_importance))]
   top_k_importance <- feature_importance[top_k_indices]
   
   motif_names <- colnames(as.matrix(svm_model$SV))[top_k_indices]
