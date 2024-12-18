0e521e837ed3e959c60de824e19a4a04b95ec7e6
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-18 13:52:47 +0100

    Update models2dataframe

--- models2dataframe.R
+++ models2dataframe.R
@@ -1,50 +1,50 @@
 models2dataframe <- function(regression_list, k = 5, alpha = NULL){
-  
+
   regression_type <- names(regression_list)
-  
-  if (("simple" %in% regression_type && is.null(alpha))) {
+
+  if (("LM" %in% regression_type && is.null(alpha))) {
     stop("\u03B1 MUST be defined when simple regression is employed")
     }
-  
-  if((!("simple" %in% regression_type) && !is.null(alpha))){
+
+  if((!("LM" %in% regression_type) && !is.null(alpha))){
     stop("\u03B1 MUST NOT be defined when simple regression is employed")
     }
-  
+
   output <- list()
   for (idx in seq_along(1:length(regression_list))){
-    
+
     model_name <- names(regression_list[idx])
     models_list <- regression_list[[idx]]
-    
-    if (model_name == "lasso"){
+
+    if (model_name == "Randomized_lasso"){
       out_list <- lapply(X = seq_along(1:length(models_list)), FUN = function(condition){
         lasso_sublist <- models_list[condition]
         output_df <- unpack_lasso(lasso_sublist, k)})
-      
+
       final_df <- do.call(rbind,out_list)
       output[[model_name]] <- final_df
-      
-    } else if (model_name == "pls") {
+
+    } else if (model_name == "PLS") {
       out_list <- lapply(X = seq_along(1:length(models_list)), FUN = function(condition){
         pls_sublist <- models_list[condition]
         output_df <- unpack_pls(pls_sublist, k)})
-      
+
       final_df <- do.call(rbind,out_list)
       output[[model_name]] <- final_df
-      
-    } else if(model_name == "svm"){
+
+    } else if(model_name == "SVM"){
       out_list <- lapply(X = seq_along(1:length(models_list)), FUN = function(condition){
         svm_sublist <- models_list[condition]
         output_df <- unpack_svm(svm_sublist, k)})
-      
+
       final_df <- do.call(rbind,out_list)
       output[[model_name]] <- final_df
-      
-    } else if(model_name == "simple"){
+
+    } else if(model_name == "LM"){
       out_list <- lapply(X = seq_along(1:length(models_list)), FUN = function(condition){
         simple_sublist <- models_list[condition]
         output_df <- unpack_simple(simple_sublist, k, alpha = alpha)})
-      
+
       final_df <- do.call(rbind,out_list)
       output[[model_name]] <- final_df
     }
