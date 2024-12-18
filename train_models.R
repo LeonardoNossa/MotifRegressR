e69f139b20c8ba7317f78eb2317be6bc0eef60fc
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-18 13:53:25 +0100

    Update train_models

--- train_models.R
+++ train_models.R
@@ -1,34 +1,46 @@
-train_models <- function(S, EXPN, conditions, regression = "simple") {
+train_models <- function(S, EXPN, conditions, regression = "LM") {
   regression_func <- switch(
     as.character(regression),
-    "lasso" = Regression_lasso,
-    "pls" = Regression_pls,
-    "svm" = Regression_svm,
-    "simple" = Regression_simple,
+    "Randomized_lasso" = Regression_lasso,
+    "PLS" = Regression_pls,
+    "SVM" = Regression_svm,
+    "LM" = Regression_simple,
     NULL
   )
-  
+
   if (is.null(regression_func)) {
     stop("Please, choose among the following options: \n- lasso\n- pls\n- svm\n- simple")
   }
-  
-  if (all(is.character(conditions)) && conditions == 'all') {
-    output_all <- sapply(
-      X = seq_along(1:ncol(EXPN)), FUN = function(condition_id) {
-        output <- regression_func(S, EXPN, condition_id)
-        return(output)},
-      simplify = FALSE
-    )
-    names(output_all) <- colnames(EXPN)
-  } else {
-    output_all <- sapply(
-      X = conditions, FUN = function(condition_id) {
-        output <- regression_func(S, EXPN, condition_id)
-        return(output)},
-      simplify = FALSE
-    )
-    names(output_all) <- colnames(EXPN)[conditions]
+
+  if (is.character(conditions)){
+    if (all(conditions == "all")){
+      conditions <- seq_len(ncol(EXPN))
+    } else {
+      conditions <- match(conditions, colnames(EXPN))
+      if (any(is.na(conditions))){
+        stop("One or more column names in 'conditions' do not exist in 'EXPN'")
+      }
+    }
   }
-  
+
+  output_all <- sapply(
+    X = conditions, FUN = function(condition_id){
+      output <- regression_func(S, EXPN, condition_id)
+      return(output)
+    },
+    simplify = FALSE
+  )
+  names(output_all) <- colnames(EXPN)[conditions]
+
   return(output_all)
 }
+
+
+
+
+
+
+
+
+
+
