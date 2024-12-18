8ae6685db1c5335c4a697377945c413c46c6a09f
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-18 10:18:23 +0100

    Update documentation AllScores.R
    Fix documentation for latest package updates

--- AllScores.R
+++ AllScores.R
@@ -2,14 +2,15 @@
 #'
 #' This function calculates the maximum scores for a list of Position Weight 
 #' Matrices (PWMs) across a set of DNA sequences using parallel computation. 
-#' For each PWM, it computes the maximum score for every sequence and returns 
-#' a matrix of scores.
+#' For each PWM, it computes the maximum score for every upstream sequence 
+#' and returns a matrix of scores.
 #'
 #' @param PWMs A list of Position Weight Matrices (PWMs). Each PWM in the list
-#'  should be a numeric matrix with 4 rows (corresponding to A, C, G, T).
-#' @param dataSeq A named character vector containing DNA sequences to be 
-#' scored. Each sequence must consist of the characters "A", "C", "G", and "T". 
-#' The names of the vector will be used as row names in the output.
+#'  should be a numeric matrix with 4 columns (corresponding to A, C, G, T).
+#' @param dataSeq A matrix containing DNA sequences to be 
+#' scored and their corresponding chromosome. 
+#' Each sequence must consist of the characters "A", "C", "G", and "T". 
+#' The rownames (gene_IDs) of the matrix will be used as row names in the output.
 #'
 #' @return A numeric matrix where:
 #' \item{Rows}{Correspond to the DNA sequences (named from `dataSeq`).}
