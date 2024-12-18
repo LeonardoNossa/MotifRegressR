b5fb98021dd14587f75945a5f36f241880e4fb74
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-18 13:51:33 +0100

    Update consensus motif and heatmap
    Update input and heatmap also for conditions, Brillii vult

--- consensus_motifs.R
+++ consensus_motifs.R
@@ -1,33 +1,43 @@
-consensus_motifs <- function(...) {
-  inputs <- list(...)
-  final_inputs <- list()
-  for (object in inputs) {
+consensus_motifs <- function(df_list, by_regressor = TRUE) {
+  for (object in df_list) {
+
     if (!is(object, "data.frame")) {
-      regressor_type <- retrieve_regressor_type(object[[1]])
-      object <- models2dataframe(object, regressor_type, k = 5)
-      final_inputs <- append(final_inputs, list(object))
-    } else {
-      final_inputs <- append(final_inputs, list(object))
+      err_msg <- paste0("Please, the provided list must contain only",
+                        " dataframes, consider providing the output of",
+                        " 'models2dataframe' function.")
+      stop(err_msg)
     }
   }
-  
+
   output <- list()
-  
-  conditions <- unique(unlist(final_inputs)[grepl("^conditions",
-                                                  names(unlist(final_inputs)))])
-  for (condition in conditions) {
-    all_motifs_in_condition <- lapply(X = final_inputs, FUN = function(df) {
-      subset_df <- df$motifs[df$conditions == condition]
-      return(subset_df)
-    })
-    all_motifs_in_condition <- unlist(all_motifs_in_condition)
-    sorted_motifs_summary <- sort(table(all_motifs_in_condition),
-                                  decreasing = TRUE)
-    sorted_motifs_summary <- (sorted_motifs_summary/length(inputs))*100
-    output[[condition]] <- as.vector(sorted_motifs_summary)
-    names(output[[condition]]) <- names(sorted_motifs_summary)
-    
+  conditions <- unique(unlist(df_list)[grepl("conditions",
+                                             names(unlist(df_list)))])
+  if (by_regressor) {
+    for (condition in conditions) {
+      all_motifs_in_condition <- lapply(X = df_list, FUN = function(df) {
+        subset_df <- df$motifs[df$conditions == condition]
+        return(subset_df)
+      })
+      all_motifs_in_condition <- unlist(all_motifs_in_condition)
+      sorted_motifs_summary <- sort(table(all_motifs_in_condition),
+                                    decreasing = TRUE)
+      sorted_motifs_summary <- ((sorted_motifs_summary/length(df_list))*100)
+      output[[condition]] <- as.vector(sorted_motifs_summary)
+      names(output[[condition]]) <- names(sorted_motifs_summary)
+
+    }
+  } else {
+    regressions <- names(df_list)
+    for (i in seq_len(length(df_list))) {
+      df_regr <- df_list[[i]]
+      sorted_motifs_summary <- sort(table(df_regr$motifs), decreasing = TRUE)
+      sorted_motifs_summary <- ((sorted_motifs_summary/length(conditions))*100)
+      output[[regressions[i]]] <- as.vector(sorted_motifs_summary)
+      names(output[[i]]) <- names(sorted_motifs_summary)
+    }
+
+
   }
-  
+
   return(output)
 }
--- consensus_motifs_heatmap.R
+++ consensus_motifs_heatmap.R
@@ -1,4 +1,16 @@
 consensus_motifs_heatmap <- function(consensus_output) {
+
+  if ("LM" %in% names(consensus_output) || "SVM" %in% names(consensus_output) ||
+      "PLS" %in% names(consensus_output) || "Randomized_lasso" %in% consensus_output) {
+    x_label <- "Regressors"
+    title <- "Heatmap of Motif Inclusivity Across Conditions"
+  }
+  else {
+    x_label <- "Conditions"
+    title <- "Heatmap of Motif Inclusivity Across Regression Models"
+
+  }
+
   all_data <- lapply(names(consensus_output), function(condition) {
     data.frame(
       Condition = condition,
@@ -7,10 +19,10 @@ consensus_motifs_heatmap <- function(consensus_output) {
     )
   })
   long_df <- do.call(rbind, all_data)
-  
+
   long_df$Condition <- factor(long_df$Condition, levels = unique(long_df$Condition))
   long_df$Motif <- factor(long_df$Motif, levels = unique(long_df$Motif))
-  
+
   p <- ggplot(long_df, aes(x = Condition, y = Motif, fill = Frequency)) +
     geom_tile(color = "white") +
     scale_fill_gradient(
@@ -32,27 +44,27 @@ consensus_motifs_heatmap <- function(consensus_output) {
       axis.ticks = element_blank()
     ) +
     labs(
-      x = "Conditions",
+      x = x_label,
       y = "Motifs",
-      title = "Heatmap of Motif Inclusivity Across Regression Models"
+      title = title
     ) +
     coord_fixed(ratio = length(unique(long_df$Condition)) / length(unique(long_df$Motif)))
-  
+
   p +
-    
+
     geom_segment(aes(x = as.numeric(Condition) - 0.5, xend = as.numeric(Condition) - 0.5,
                      y = min(as.numeric(Motif)) - 0.5, yend = max(as.numeric(Motif)) + 0.5),
                  color = "black") +
-    
-    
+
+
     geom_segment(aes(x = min(as.numeric(Condition)) - 0.5, xend = max(as.numeric(Condition)) + 0.5,
                      y = as.numeric(Motif) - 0.5, yend = as.numeric(Motif) - 0.5),
                  color = "black") +
-    
+
     geom_segment(aes(x = min(as.numeric(Condition)) - 0.5, xend = max(as.numeric(Condition)) + 0.5,
                      y = max(as.numeric(Motif)) + 0.5, yend = max(as.numeric(Motif)) + 0.5),
                  color = "black") +
-    
+
     geom_segment(aes(x = max(as.numeric(Condition)) + 0.5, xend = max(as.numeric(Condition)) + 0.5,
                      y = min(as.numeric(Motif)) - 0.5, yend = max(as.numeric(Motif)) + 0.5),
                  color = "black")
