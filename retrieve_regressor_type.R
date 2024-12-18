4afc0ed12aa1b9cfa9c7962c17fb5942a5db157b
Elena <129421537+elenasasso@users.noreply.github.com>
2024-12-18 22:05:08 +0100

    Update retrieve_regressor_type.R with roxygen

--- retrieve_regressor_type.R
+++ retrieve_regressor_type.R
@@ -1,3 +1,21 @@
+#' Retrieve the Type of a Regression Model
+#'
+#' This function determines the type of a given regression model based on its 
+#' class.
+#'
+#' @param model An object representing a regression model. The model's class is
+#'  used to determine its type.
+#'
+#' @return A character string indicating the type of regression model:
+#' \itemize{
+#'   \item `"svm"` for Support Vector Machine models.
+#'   \item `"pls"` for Partial Least Squares regression models.
+#'   \item `"lasso"` for Lasso regression models (stored as 
+#'   `SummarizedExperiment` objects).
+#'   \item `"simple"` for simple linear regression models.
+#' }
+#'
+#' @export
 retrieve_regressor_type <- function(model) {
   if (class(model) == "svm") {
     return("svm")
@@ -5,7 +23,7 @@ retrieve_regressor_type <- function(model) {
     return("pls")
   } else if (class(model) == "SummarizedExperiment") {
     return("lasso")
-  } else if (class(model) == "lm"){
+  } else if (class(model) == "lm") {
     return("simple")
   }
 }
