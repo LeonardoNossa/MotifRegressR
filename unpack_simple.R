53001e1b63667fa57057099f41a1011dadbee85b
Teodor Devda <teodordevda@yahoo.com>
2024-12-21 10:21:15 +0100

    Add files via upload
    Updated unpack_simple

--- unpack_simple.R
+++ unpack_simple.R
@@ -20,34 +20,35 @@
 #'
 #' @export
 unpack_simple <- function(simple_sublist, k, alpha){
-  simple_model <- simple_sublist[[1]]
-  
-  coefficients <- summary(simple_model)$coefficients
-  
-  filtered_coefficients <- coefficients[coefficients[,"Pr(>|t|)"] <= alpha,]
-  
-  if (length(filtered_coefficients) == 0) {
-    return(NULL)
-  }
-  
-  ordered_filtered_coefficients <- 
-    filtered_coefficients[order(filtered_coefficients[,"Pr(>|t|)"], 
-                                decreasing = FALSE), , drop = FALSE]
-  ordered_filtered_coefficients <- 
-    ordered_filtered_coefficients[rownames(ordered_filtered_coefficients) 
-                                  != "(Intercept)",]
-  top_k_coeffients <- 
-    ordered_filtered_coefficients[1:min(k, 
-                                        nrow(ordered_filtered_coefficients)), 
-                                  , drop = FALSE]
-  condition_name <- rep(names(simple_sublist), times = nrow(top_k_coeffients))
-  
-  output_df <- data.frame(
-    motifs = rownames(top_k_coeffients),
-    conditions = condition_name,
-    coef = as.vector(top_k_coeffients[,"Estimate"])
-  )
-  output_df$motifs <- gsub("`", "", output_df$motifs)
-  rownames(output_df) <- seq_along(1:nrow(output_df))
-  return(output_df)
+    simple_model <- simple_sublist[[1]]
+    
+    coefficients <- summary(simple_model)$coefficients
+    
+    filtered_coefficients <- coefficients[coefficients[,"Pr(>|t|)"] <= alpha,]
+    
+    if (length(filtered_coefficients) == 0) {
+        return(NULL)
+    }
+    
+    ordered_filtered_coefficients <- 
+        filtered_coefficients[order(abs(filtered_coefficients[,"Estimate"]),
+                                    decreasing = TRUE), , drop = FALSE]
+       
+    ordered_filtered_coefficients <- 
+        ordered_filtered_coefficients[rownames(ordered_filtered_coefficients) 
+                                      != "(Intercept)",]
+    top_k_coeffients <- 
+        ordered_filtered_coefficients[1:min(k, 
+                                            nrow(ordered_filtered_coefficients)), 
+                                      , drop = FALSE]
+    condition_name <- rep(names(simple_sublist), times = nrow(top_k_coeffients))
+    
+    output_df <- data.frame(
+        motifs = rownames(top_k_coeffients),
+        conditions = condition_name,
+        coef = as.vector(top_k_coeffients[,"Estimate"])
+    )
+    output_df$motifs <- gsub("`", "", output_df$motifs)
+    rownames(output_df) <- seq_along(1:nrow(output_df))
+    return(output_df)
 }
