4faa45650992ddc6452fd00b47d20b55c9c534fb
Marco Cominelli <161143279+marco-cominelli01@users.noreply.github.com>
2024-12-21 15:20:37 +0100

    Update unpack_pls.R
    Coefficients are now sorted (and extracted) according to their absolute value.

--- unpack_pls.R
+++ unpack_pls.R
@@ -15,24 +15,25 @@
 #' }
 #'
 #' @export
-unpack_pls <- function(pls_sublist, k){
-  pls_model <- pls_sublist[[1]]
-  coefficients <- coef(pls_model)
-  motif_names <- rownames(coefficients)
-  
-  condition_name <- rep(x = names(pls_sublist),
-                        times = min(k, length(coefficients)))
-  
-  ranking_idxs <- order(coefficients, decreasing = TRUE)
-  coefficients <- coefficients[ranking_idxs]
-  motif_names <- motif_names[ranking_idxs]
-  
-  top_k <- coefficients[1:min(k, length(coefficients))]
-  top_k_names <- motif_names[1:min(k, length(coefficients))]
-  
-  output_df <- data.frame(motifs = top_k_names,
-                          conditions = condition_name,
-                          coef = top_k)
-  rownames(output_df) <- seq_along(1:nrow(output_df))
-  return(output_df)
+unpack_pls <- function(pls_sublist, k) {
+    pls_model <- pls_sublist[[1]]
+    coefficients <- coef(pls_model)
+    motif_names <- rownames(coefficients)
+    
+    condition_name <- rep(x = names(pls_sublist),
+                          times = min(k, length(coefficients)))
+    
+    ranking_idxs <- order(abs(coefficients), decreasing = TRUE)
+    coefficients <- coefficients[ranking_idxs]
+    motif_names <- motif_names[ranking_idxs]
+    
+    top_k <- coefficients[1:min(k, length(coefficients))]
+    top_k_names <- motif_names[1:min(k, length(coefficients))]
+    
+    output_df <- data.frame(motifs = top_k_names,
+                            conditions = condition_name,
+                            coef = top_k)
+    rownames(output_df) <- seq_along(1:nrow(output_df))
+    
+    return(output_df)
 }
