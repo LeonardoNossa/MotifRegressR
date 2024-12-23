a38f9608c37767e7583f01991d66e2181b1ca57c
Teodor Devda <teodordevda@yahoo.com>
2024-12-23 13:54:19 +0100

    Add files via upload

---
+++ nucleotide_frequencies.R
@@ -0,0 +1,18 @@
+#' Starting from FASTA file obtain the nucleotide frequencies
+#'
+#' This function takes in input FASTA fie and compute the freqeucnies of each 
+#' nucleotide.
+#' @param fasta The path to the input FASTA file 
+#' @return A named vector with frequencies for each nucleotide.
+nucleotide_frequencies <- function(fasta){
+    FASTA <- Biostrings::readDNAStringSet(fasta)
+    freq <-  Biostrings::alphabetFrequency(FASTA, baseOnly = TRUE)[, c("A", "C", "G", "T")]
+    if (is.vector(freq)) {
+        frequencies <- freq / sum(freq)
+    } else {
+        frequencies <- colSums(freq) / sum(freq)
+    }
+    return(frequencies)
+}
+
+
