0103b6deeb7f40733056b204f6df5f65c3cc480c
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-26 16:31:09 +0100

    Update dimensionality reduction

--- plot_dimensionality_reduction.R
+++ plot_dimensionality_reduction.R
@@ -1,21 +1,21 @@
 #' Plot Dimensionality Reduction
 #'
-#' This function performs dimensionality reduction on gene expression data 
+#' This function performs dimensionality reduction on gene expression data
 #' and visualizes the results using `plotly`, integrating metadata information
-#' whenever possible. 
+#' whenever possible.
 #' It supports multiple methods, including PCA, MDS, t-SNE, and UMAP.
 #'
-#' @param compendium A data frame or matrix where rows represent genes 
+#' @param compendium A data frame or matrix where rows represent genes
 #' and columns represent samples.
-#' @param metadata A data frame containing metadata for the samples, 
-#' where each row corresponds to a sample in `compendium`, 
+#' @param metadata A data frame containing metadata for the samples,
+#' where each row corresponds to a sample in `compendium`,
 #' if metadata for that sample are available.
-#' @param color_by A string specifying the column name in `metadata` 
+#' @param color_by A string specifying the column name in `metadata`
 #' to be used for coloring points in the plot.
-#' @param method A string specifying the dimensionality reduction method. 
+#' @param method A string specifying the dimensionality reduction method.
 #' Options are "PCA", "MDS", "t-SNE", or "UMAP". Defaults to "PCA".
 #'
-#' @return A `plotly` object visualizing the dimensionality reduction result, 
+#' @return A `plotly` object visualizing the dimensionality reduction result,
 #' with interactive tooltips, showing metadata information.
 #'
 #' @details
@@ -33,15 +33,19 @@
 #' @importFrom umap umap
 #' @importFrom plotly plot_ly layout
 #' @export
-plot_dimensionality_reduction <- function(compendium, metadata, color_by, method = "PCA"){
-  
+plot_dimensionality_reduction <- function(compendium, metadata = NULL, color_by = NULL, method = "PCA"){
+
+  if((!is.null(metadata) && is.null(color_by))||(is.null(metadata)&&!is.null(color_by))){
+    stop("When either metadata or color_by are NULL, both of them MUST be NULL")
+  }
+
   `%>%` <- magrittr::`%>%`
   data_transposed <- t(compendium)
-  
+
   reduction_result <- NULL
   explained_variance <- NULL
   axis_labels <- c("Dimension 1", "Dimension 2")
-  
+
   if (method == "PCA") {
     pca <- stats::prcomp(data_transposed, center = TRUE, scale. = TRUE)
     reduction_result <- suppressWarnings(tibble::as_tibble(pca$x[, 1:2]))
@@ -67,26 +71,64 @@ plot_dimensionality_reduction <- function(compendium, metadata, color_by, method
   } else {
     stop("Method not supported. Please choose among 'PCA', 'MDS', 't-SNE', or 'UMAP'.")
   }
-  
+
   reduction_result <- reduction_result %>% dplyr::mutate(Sample = rownames(data_transposed))
-  
+
+  if (is.null(metadata) && is.null(color_by)) {
+    reduction_result <- reduction_result %>% dplyr::mutate(Tooltip = paste0("run_accession: ",colnames(compendium)))
+
+    fig <- plotly::plot_ly(
+      data = reduction_result,
+      x = ~ .data[[colnames(reduction_result)[1]]],
+      y = ~ .data[[colnames(reduction_result)[2]]],
+      type = 'scatter', mode = 'markers',
+      marker = list(size = 10, color = 'blue'),
+      text = ~Tooltip,
+      hoverinfo = "text"
+    ) %>% plotly::layout(
+      title = paste(method, 'on Conditions'),
+      xaxis = list(title = axis_labels[1]),
+      yaxis = list(title = axis_labels[2]),
+      legend = list(orientation = 'h', x = 0, y = -0.2)
+    )
+
+    fig <- htmlwidgets::onRender(fig, "
+  function(el, x) {
+    el.on('plotly_selected', function(data) {
+      if (data && data.points) {
+        var selected_accessions = data.points.map(pt => {
+          var tooltip = pt.text || '';
+          var accession_match = tooltip.match(/run_accession: ([^<]+?)(?=<br>|$)/);
+          return accession_match ? accession_match[1] : null;
+        }).filter(accession => accession !== null);
+        Shiny.onInputChange('selected_run_accession', selected_accessions);
+      } else {
+        Shiny.onInputChange('selected_run_accession', null);
+      }
+    });
+  }
+");
+
+    return(fig)
+  }
+
   if (!"Sample" %in% colnames(metadata)) {
     metadata <- metadata %>% tibble::rownames_to_column(var = "Sample")
   }
-  
+
   if (!color_by %in% colnames(metadata)) {
     stop(paste("Column", color_by, "does not exist in 'metadata' data.frame"))
   }
-  
+
   reduction_data <- reduction_result %>%
     dplyr::left_join(metadata, by = "Sample")
-  
+
   unique_groups <- unique(reduction_data[[color_by]])
   num_groups <- length(unique_groups)
-  
+
   palette_colors <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(min(8, num_groups), "Set1"))(num_groups)
   color_mapping <- stats::setNames(palette_colors, unique_groups)
-  
+
   tooltip_text <- apply(metadata, 1, function(row) {
     to_keep <- c("study_accession", "tax_id", "scientific_name", "instrument_model", "sample_title")
     paste(
@@ -95,10 +137,10 @@ plot_dimensionality_reduction <- function(compendium, metadata, color_by, method
       sep = "<br>"
     )
   })
-  
+
   fig <- plotly::plot_ly(
     data = reduction_data,
-    x = ~ .data[[colnames(reduction_result)[1]]], 
+    x = ~ .data[[colnames(reduction_result)[1]]],
     y = ~ .data[[colnames(reduction_result)[2]]],
     type = 'scatter', mode = 'markers',
     color = ~.data[[color_by]],
@@ -119,7 +161,7 @@ plot_dimensionality_reduction <- function(compendium, metadata, color_by, method
       tracegroupgap = 5
     )
   )
-  
+
   fig <- htmlwidgets::onRender(fig, "
   function(el, x) {
     el.on('plotly_selected', function(data) {
@@ -136,7 +178,8 @@ plot_dimensionality_reduction <- function(compendium, metadata, color_by, method
     });
   }
 ");
-  
+
   return(fig)
 }
+
 
