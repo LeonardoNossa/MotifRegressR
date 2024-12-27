b039c987ee7e17eb4db0971778c04960d12b4e2f
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-27 18:05:47 +0100

    Update Regression_KNN.R

--- Regression_KNN.R
+++ Regression_KNN.R
@@ -1,42 +1,44 @@
 #' K-Nearest Neighbors (KNN) Regression
 #'
 #' This function performs KNN regression using the \code{caret} package.
-#' Best model is evaluated using the  Recursive Feature Elimination.
-#' Computation of the best model is parallelized using \code{future}
-#' and \code{doFuture} packages
+#' The best model is evaluated using Recursive Feature Elimination (RFE).
+#' Computation of the best model is parallelized using \code{doParallel}.
 #'
 #' @param Scores A matrix or data frame of scores to be used as predictors in
-#' the SVM regression. This is internally converted to a matrix.
+#' the KNN regression. This is internally converted to a matrix.
 #' @param TPMs A data frame or matrix of TPM (Transcripts Per Million) values.
 #' Columns correspond to different conditions or samples.
 #' @param Condition A character string specifying the column name in \code{TPMs}
 #' corresponding to the condition to be analyzed.
 #'
-#' @return An object of class \code{RFE} representing the fitted SVM regression model.
+#' @return An object of class \code{RFE} representing the fitted KNN regression model.
 #'
-#' @importFrom future multisession plan future value
+#' @details This function uses Recursive Feature Elimination (RFE) to select
+#' the best set of predictors for KNN regression. Cross-validation is employed
+#' to evaluate model performance. The computation is parallelized using
+#' \code{doParallel} to improve efficiency.
+#'
+#' @importFrom parallel makeCluster stopCluster
+#' @importFrom doParallel registerDoParallel
 #' @importFrom caret rfeControl caretFuncs rfe
-#' @importFrom doFuture registerDoFuture
 Regression_KNN <- function(Scores, TPMs, Condition) {
 
-  doFuture::registerDoFuture()
-  future::plan(future::multisession, workers = 10)
-
-  rfe_control <- caret::rfeControl(functions = caret::caretFuncs,
-                                   method = "cv",
-                                   number = 3,
-                                   allowParallel = TRUE)
-
-  rfe_results <- future::future({
-    caret::rfe(
-      x = Scores,
-      y = TPMs[, Condition],
-      sizes = c(1:(ncol(Scores) - 1)),
-      rfeControl = rfe_control,
-      method = "knn"
-    )
-  }, seed = TRUE)
-
-  rfe_results <- future::value(rfe_results)
+  num_cores <- 10
+  cl <- parallel::makeCluster(num_cores)
+  doParallel::registerDoParallel(cl)
+  rfe_control <- suppressWarnings(caret::rfeControl(
+    functions = caret::caretFuncs,
+    method = "cv",
+    number = 3,
+    allowParallel = TRUE
+  ))
+  rfe_results <- caret::rfe(
+    x = Scores,
+    y = TPMs[, Condition],
+    sizes = c(1:(ncol(Scores) - 1)),
+    rfeControl = rfe_control,
+    method = "knn"
+  )
+  suppressWarnings(parallel::stopCluster(cl))
   return(rfe_results)
 }
