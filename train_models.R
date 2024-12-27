a56e6fb2e71589608af72b5c5de64ccc6e63d9d1
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2024-12-27 15:12:25 +0100

    Update train_models.R

--- train_models.R
+++ train_models.R
@@ -5,7 +5,7 @@
 #'
 #' @param S A numeric matrix or data frame of scores
 #' @param EXPN A numeric matrix or data frame of expression values
-#' @param conditions A vector specifying the conditions to be analyzed. Can be 
+#' @param conditions A vector specifying the conditions to be analyzed. Can be
 #' column indices or names in `EXPN`. Use `"all"` to include all columns.
 #' @param regression A character string indicating the regression method to use.
 #'  Options are:
@@ -14,12 +14,11 @@
 #'   \item `"PLS"` for Partial Least Squares regression.
 #'   \item `"SVM"` for Support Vector Machine regression.
 #'   \item `"LM"` for simple linear regression.
+#'   \item `"KNN"` for KNN regression.
 #' }
 #'
 #' @return A named list where each element corresponds to a regression model for
 #'  a specific condition.
-#'
-#' @export
 train_models <- function(S, EXPN, conditions, regression = "LM") {
   regression_func <- switch(
     as.character(regression),
@@ -27,14 +26,15 @@ train_models <- function(S, EXPN, conditions, regression = "LM") {
     "PLS" = Regression_pls,
     "SVM" = Regression_svm,
     "LM" = Regression_simple,
+    "KNN" = Regression_KNN,
     NULL
   )
-  
+
   if (is.null(regression_func)) {
     stop("Please, choose among the following options:
-         \n- lasso\n- pls\n- svm\n- simple")
+         \n- Randomized_lasso\n- PLS\n- SVM\n- LM\n- KNN")
   }
-  
+
   if (is.character(conditions)){
     if (all(conditions == "all")){
       conditions <- seq_len(ncol(EXPN))
@@ -45,7 +45,7 @@ train_models <- function(S, EXPN, conditions, regression = "LM") {
       }
     }
   }
-  
+
   output_all <- sapply(
     X = conditions, FUN = function(condition_id){
       output <- regression_func(S, EXPN, condition_id)
@@ -54,6 +54,6 @@ train_models <- function(S, EXPN, conditions, regression = "LM") {
     simplify = FALSE
   )
   names(output_all) <- colnames(EXPN)[conditions]
-  
+
   return(output_all)
 }
