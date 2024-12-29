933b4aba96ebc9ab3f503fae74550310bb7d1407
Elena <129421537+elenasasso@users.noreply.github.com>
2024-12-29 15:58:13 +0100

    Update retrieve_frequencies.R

--- R/retrieve_frequencies.R
+++ R/retrieve_frequencies.R
@@ -2,22 +2,24 @@
 #'
 #' This function takes in input FASTA file and computes the frequencies of each 
 #' nucleotide.
-#' @param fasta The path to the input FASTA file 
+#' 
+#' @param fasta The path to the input FASTA file
+#'  
 #' @return A named vector with frequencies for each nucleotide
 #' (in this order A,C,G,T)
+#' 
 #' @importFrom Biostrings readDNAStringSet
 #' @importFrom Biostrings alphabetFrequency
 #' @export
 retrieve_frequencies <- function(fasta){
-    FASTA <- Biostrings::readDNAStringSet(fasta)
-    freq <-  Biostrings::alphabetFrequency(FASTA, baseOnly = TRUE)[, c("A", "C", "G", "T")]
-    if (is.vector(freq)) {
-        frequencies <- freq / sum(freq)
-    } else {
-        frequencies <- colSums(freq) / sum(freq)
-    }
-    return(frequencies)
+  FASTA <- Biostrings::readDNAStringSet(fasta)
+  freq <-  Biostrings::alphabetFrequency(FASTA, 
+                                         baseOnly = TRUE)[, 
+                                                          c("A", "C", "G", "T")]
+  if (is.vector(freq)) {
+    frequencies <- freq / sum(freq)
+  } else {
+    frequencies <- colSums(freq) / sum(freq)
+  }
+  return(frequencies)
 }
-
-
-
