979f7b878b132616dbb05e5d99912a3d181f192e
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2025-01-05 09:17:49 +0100

    Update principal_comps.R documentation

--- R/principal_comps.R
+++ R/principal_comps.R
@@ -1,23 +1,21 @@
 #' Perform Principal Component Analysis (PCA)
 #'
-#' This function performs Principal Component Analysis (PCA) on a given matrix 
-#' and extracts the components needed to explain a specified percentage of the 
+#' This function performs Principal Component Analysis (PCA) on a given matrix
+#' and extracts the components needed to explain a specified percentage of the
 #' variance.
 #'
-#' @param matrix A numeric matrix to perform PCA on. Each row represents an 
-#' observation, and each column represents a variable. If `axis = 1`, rows and 
-#' columns are transposed.
+#' @param matrix A numeric matrix to perform PCA on. Each row represents an
+#' observation, and each column represents a variable.
 #' @param explained_var A numeric value (default: 95) indicating the percentage
 #' of variance that the retained principal components should explain.
-#' @param axis An integer (default: 2) indicating whether to analyze the input 
-#' matrix as is (`axis = 2`) or transpose it (`axis = 1`).
+#' @param axis An integer (default: 2) indicating whether to perform
+#' PCA on the columns (`axis = 2`) or on the rows of the matrix (`axis = 1`).
 #' @param scale A logical value (default: TRUE) indicating whether the variables
 #' should be scaled to unit variance before performing PCA.
 #'
-#' @return A numeric matrix containing the coordinates of the input data 
-#' projected onto the principal components that explain the specified variance. 
-#' The output matrix has dimensions consistent with the transposition specified 
-#' by `axis`.
+#' @return A numeric matrix containing the coordinates of the input data
+#' projected onto the principal components that explain the specified variance.
+#'
 #'
 #' @import FactoMineR
 #' @export
