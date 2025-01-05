06300c0db6d67b5524c3935335dc1857e31d4b6f
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2025-01-05 22:38:14 +0100

    Update package description

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,24 @@
 #' @title MotifRegressR: Perform motif regression on gene expression conditions
 #'
-#' @description This R package facilitates motif analysis
-#' for gene expression studies. It integrates upstream DNA sequence information,
-#' gene expression profiles, and Position Frequency Matrices (PFMs)
-#' to identify regulatory motifs influencing gene expression.
-#' Users can import PFMs from JASPAR or custom sources,
-#' annotate genomic sequences with transcription start sites (TSS),
-#' and retrieve metadata of gene expression conditions.
-#' The package scores motifs across genes, aggregates the results,
-#' and performs regression analysis to determine the most impactful motifs
-#' under different experimental conditions. Visualization functions finally
-#' help the user in the sense-making step of the analyses.
+#' @description MotifRegressR allows its users to perform DNA motifs regression
+#' in the context of gene expression studies, allowing the investigation
+#' of the most impactful motifs over different samples.
+#' This is done by integrating gene upstream sequences (promoters)
+#' with Position Weight Matrices (PWMs) of the chosen organism.
+#' By sliding the PWMs over each gene, we assign to each gene
+#' the maximum score obtained for each DNA motif
+#' and then build the matrix of scores S.
+#' Users can import PFMs from JASPAR or custom sources (via a MEME file)
+#' and obtain the upstream sequences by proving a .gff and a FASTA file as input.
+#' Users can also upload gene expression compendia containing
+#' gene expression levels across different conditions.
+#' If the IDs associated to each condition allow it,
+#' metadata can be retrieved from ENA browser to obtain more
+#' information regarding the samples. MotifRegressR then performs
+#' regression analysis and determines the most impactful motifs for
+#' the different tested conditions. Downstream steps, mainly represented
+#' by visualization functions finally help the user in the sense-making steps
+#' of the analyses.
 #' @name MotifRegressR
 #' @keywords package
 "_PACKAGE"
