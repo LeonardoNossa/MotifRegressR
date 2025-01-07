cb262b69707fe82a59324665544d6c22ee9bfbcf
Gabriele Oliveto <161151189+Gab-23@users.noreply.github.com>
2025-01-07 14:31:58 +0100

    update extdata documentation

---
+++ R/exdata_documentation.R
@@ -0,0 +1,53 @@
+#' Example Data for Drosophila melanogaster
+#'
+#' This dataset includes genomic data for Drosophila melanogaster, consisting of a nucleotide sequence file and a genomic annotation file.
+#'
+#' @section Files:
+#' \describe{
+#'   \item{\code{GCF_000001215.4_Release_6_plus_ISO1.fna}}{FASTA file containing nucleotide sequences for the Drosophila melanogaster genome.}
+#'   \item{\code{d_melanogaster_genomic.gff}}{GFF file containing annotations for the genome, including gene positions and features.}
+#' }
+#' @source Genome data for \emph{Drosophila melanogaster} obtained from NCBI.
+#' @name D.melanogaster
+NULL
+#' Example Data for Caenorhabditis elegans
+#'
+#' This dataset includes genomic data for Caenorhabditis elegans, consisting of a nucleotide sequence file and a genomic annotation file.
+#'
+#' @section Files:
+#' \describe{
+#'   \item{\code{GCF_000002985.6_WBcel235_genomic.fna}}{FASTA file containing nucleotide sequences for the Caenorhabditis elegans genome.}
+#'   \item{\code{c_elegans_genomic.gff}}{GFF file containing annotations for the genome, including gene positions and features.}
+#' }
+#' @source Genome data for \emph{Caenorhabditis elegans} obtained from NCBI.
+#' @name C.elegans
+NULL
+#' Example Data for Escherichia coli
+#'
+#' This dataset includes genomic data for Escherichia coli, consisting of a nucleotide sequence file, a genomic annotation file,
+#' and additional data files related to transcription units, protein information, and motifs.
+#'
+#' @section Files:
+#' \describe{
+#'   \item{\code{GCF_000005845.2_ASM584v2_genomic.fna}}{FASTA file containing nucleotide sequences for the Escherichia coli genome.}
+#'   \item{\code{GCF_000005845.2_ASM584v2_genomic.gff}}{GFF file containing annotations for the genome, including gene positions and features.}
+#'   \item{\code{compendium.xlsx}}{Excel file containing Transcripts Per Million (TPM) for different genes under certain conditions.}
+#'   \item{\code{511145_protein_info_v12_0.txt}}{Txt file containing information about E. coli protein, like protein_id, protein name, protein size, and annotation.}
+#'   \item{\code{SwissRegulon_e_coli.meme}}{Meme file containing Position Frequency Matrices (PFMs) for various transcription factor motifs.}
+#'   \item{\code{TUSet.tsv}}{Tsv file containing tabular dataset of transcription units from Escherichia coli K-12, sourced from RegulonDB 13.0. It contains detailed information about transcription units, their associated operons, genes, promoters, terminators, evidence, and confidence levels.}
+#' }
+#' @source Genome data for \emph{Escherichia coli} obtained from the coordinator of the project and NCBI.
+#' @name E.coli
+NULL
+#' Example Data for Saccharomyces cerevisiae
+#'
+#' This dataset includes genomic data for Saccharomyces cerevisiae, consisting of a nucleotide sequence file, a genomic annotation file, and gene expression data
+#'
+#' @section Files:
+#' \describe{
+#'   \item{\code{GCA_000146045.2_R64_genomic.fna}}{FASTA file containing nucleotide sequences for the Saccharomyces cerevisiae genome.}
+#'   \item{\code{GCA_000146045.2_R64_genomic.gff}}{GFF file containing annotations for the genome, including gene positions and features.}
+#' }
+#' @source Genome data for \emph{Saccharomyces cerevisiae} obtained from the coordinator of the project and NCBI.
+#' @name S.cerevisiae
+NULL
